f609bb17d7235488dc1899f2ae1a390cc02d645d
Diogo <diogo3679@gmail.com>
2021-11-25 21:29:18 +0000

    V1.0 NavigationDrawer

--- app/src/main/java/pt/isec/amov/reversi/activities/MenuActivity.kt
+++ app/src/main/java/pt/isec/amov/reversi/activities/MenuActivity.kt
@@ -1,13 +1,46 @@
 package pt.isec.amov.reversi.activities
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import pt.isec.amov.reversi.R
 
 class MenuActivity : AppCompatActivity() {
 
+    lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout : DrawerLayout
+    lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
