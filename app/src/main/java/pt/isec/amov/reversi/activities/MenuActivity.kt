dcb3e57d89f9565bc7d00478b907b1337355dbb7
Diogo <diogo3679@gmail.com>
2021-11-26 14:48:59 +0000

    Navigation Drawer - Rules | Menu

--- app/src/main/java/pt/isec/amov/reversi/activities/MenuActivity.kt
+++ app/src/main/java/pt/isec/amov/reversi/activities/MenuActivity.kt
@@ -2,7 +2,7 @@ package pt.isec.amov.reversi.activities
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.WindowManager
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -12,26 +12,32 @@ import pt.isec.amov.reversi.R
 
 class MenuActivity : AppCompatActivity() {
 
-    lateinit var toggle : ActionBarDrawerToggle
-    lateinit var drawerLayout : DrawerLayout
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
-
+    lateinit var Toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
 
+        Toolbar = findViewById(R.id.navToolbar)
+        setSupportActionBar(Toolbar)
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.nav_view)
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open,R.string.close)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         navigationView.setNavigationItemSelectedListener {
-            when(it.itemId){
+            when (it.itemId) {
 
             }
             true
@@ -39,7 +45,7 @@ class MenuActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)){
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
--- app/src/main/java/pt/isec/amov/reversi/activities/RulesActivity.kt
+++ app/src/main/java/pt/isec/amov/reversi/activities/RulesActivity.kt
@@ -2,11 +2,56 @@ package pt.isec.amov.reversi.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import pt.isec.amov.reversi.R
 
 class RulesActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var Toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rules)
+
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        Toolbar = findViewById(R.id.navToolbar)
+        setSupportActionBar(Toolbar)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.nav_view)
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
