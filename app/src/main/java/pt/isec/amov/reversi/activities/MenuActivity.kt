e3145e203ff231e453e020c46d7bd6a8c6f76e49
Diogo <diogo3679@gmail.com>
2021-11-26 19:26:37 +0000

    Animation Buttons V1.0

--- app/src/main/java/pt/isec/amov/reversi/activities/MenuActivity.kt
+++ app/src/main/java/pt/isec/amov/reversi/activities/MenuActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.WindowManager
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
 import android.widget.Button
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,8 @@ import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import pt.isec.amov.reversi.R
+import java.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 class MenuActivity : AppCompatActivity() {
 
@@ -18,7 +22,12 @@ class MenuActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
     private lateinit var Toolbar: Toolbar
-    private lateinit var btnRules : Button
+    private lateinit var btnRules: Button
+    private lateinit var btnExit : Button
+    private lateinit var btn2Off: Button
+    private lateinit var btn2On : Button
+    private lateinit var btn3On: Button
+    private lateinit var btnAnimation : AlphaAnimation
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,10 +61,37 @@ class MenuActivity : AppCompatActivity() {
         }
 
 
+        btnAnimation = AlphaAnimation(0.9F,0.5F).apply {
+            duration = 1000
+            interpolator = AccelerateInterpolator(0.1F)
+        }
+
         btnRules = findViewById(R.id.btnRules)
+        btnExit = findViewById(R.id.btnExit)
+        btn2Off = findViewById(R.id.btnModo1)
+        btn2On = findViewById(R.id.btnModo2)
+        btn3On = findViewById(R.id.btnModo3)
 
         btnRules.setOnClickListener{
+            btnRules.startAnimation(btnAnimation)
             startActivity(Intent(this,RulesActivity::class.java))
+        }
+
+        btnExit.setOnClickListener{
+            btnExit.startAnimation(btnAnimation)
+            finish()
+        }
+        btn2Off.setOnClickListener{
+            btn2Off.startAnimation(btnAnimation)
+
+        }
+        btn2On.setOnClickListener{
+            btn2On.startAnimation(btnAnimation)
+
+        }
+        btn3On.setOnClickListener{
+            btn3On.startAnimation(btnAnimation)
+
         }
 
     }
