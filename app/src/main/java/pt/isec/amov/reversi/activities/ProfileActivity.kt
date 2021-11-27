34be00f3f05e3a7e8a4a062eba6a8dc34b1f4935
Diogo <diogo3679@gmail.com>
2021-11-27 17:20:42 +0000

    Update LandScape ProfileActivity.kt

--- app/src/main/java/pt/isec/amov/reversi/activities/ProfileActivity.kt
+++ app/src/main/java/pt/isec/amov/reversi/activities/ProfileActivity.kt
@@ -22,7 +22,6 @@ class ProfileActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
