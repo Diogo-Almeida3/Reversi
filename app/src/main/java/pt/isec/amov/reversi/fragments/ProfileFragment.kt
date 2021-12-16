4174ac509a2683a124fe0c67a3cbfa6f4e9d9bfd
Diogo <diogo3679@gmail.com>
2021-12-16 12:01:16 +0000

    Camera Update

--- app/src/main/java/pt/isec/amov/reversi/fragments/ProfileFragment.kt
+++ app/src/main/java/pt/isec/amov/reversi/fragments/ProfileFragment.kt
@@ -3,6 +3,8 @@ package pt.isec.amov.reversi.fragments
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import pt.isec.amov.reversi.R
 import pt.isec.amov.reversi.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment(){
@@ -16,6 +18,14 @@ class ProfileFragment : Fragment(){
     ): View {
         binding = FragmentProfileBinding.inflate(inflater)
 
+        binding.btnEditPicture.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_cameraFragment)
+
+        }
+
+
         return binding.root
     }
+
+
 }
